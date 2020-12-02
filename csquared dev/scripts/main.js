e6ced732cf57283ad5b58ac47673d5f0552b3b72
charcoal99 <awesomekid.nb@gmail.com>
2020-12-02 14:16:36 -0600

    yes

--- csquared dev/scripts/main.js
+++ csquared dev/scripts/main.js
@@ -1,33 +1,56 @@
-//you can declair an array by
-var array = [true, 42, "and you"];
-var alt = new Array(true, 42, "and you"); //but don't do this. It is too LOOOOOOOOOONG
+//you can declair an example by
+var example = [true, 42, "and you"];
+var alt = new array(true, 42, "and you"); //but don't do this. It is too LOOOOOOOOOONG
 
 
-console.log("array item 1: " + array[0]);
-console.log("array item 2: " + array[1]);
-console.log("array item 3: " + array[2]);
+console.log("example item 1: " + example[0]);
+console.log("example item 2: " + example[1]);
+console.log("example item 3: " + example[2]);
 
-console.log("alternative array item 1: " + alt[0]);
-console.log("alternative array item 2: " + alt[1]);
-console.log("alternative array item 3: " + alt[2]);
+console.log("alternative example item 1: " + alt[0]);
+console.log("alternative example item 2: " + alt[1]);
+console.log("alternative example item 3: " + alt[2]);
 
-array[0] = false; // you can change items in an array
+example[0] = false; // you can change items in an example
 
 
-console.log(array);
+console.log(example);
 
-//nameOfArray.length will give you the length of an array
+//nameOfexample.length will give you the length of an example
 
-console.log("array length is " + array.length);
+console.log("example length is " + example.length);
 
-console.log("the first itemin array is " + array[0] + ". the last item in array is " + array[array.length - 1]);
+console.log("the first item in example is " + example[0] + ". the last item in example is " + example[example.length - 1]);
 
-//you can add on to an array by the following methods
+//you can add on to an example by the following methods
 
-array.push("Alt");
+example.push("Alt");
 
-array[array.length] = " + f4";
+example[example.length] = " + f4";
 
-array[5] = "☺";
+example[5] = "☺";
 
-console.log(array);
+//returning an item then deleting it
+example.push("elvis☺");
+console.log(example[example.length - 1]);
+example.pop();
+console.log(example);
+
+//swaping items in lists
+var temp = example[0];
+example[0] = example[1];
+eample[1] = temp;
+
+//copying arrays
+var copy = example.concat();
+
+console.log(copy);
+
+//returns the index where the element is located in the array
+console.log(example.indexOf("☺"));
+
+example = example.sort();
+
+console.log(example);
+
+//https://github.com/BearN99/Profile/blob/master/csquared%20dev/scripts/main.js
